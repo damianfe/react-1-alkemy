fbdf6f846407d4aab3c82d80e4fbf3631b4e59de
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2022-09-01 15:27:51 -0300

    favorites logic finished

--- src/App.js
+++ src/App.js
@@ -10,19 +10,58 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 //Estilos
+import './css/app.css';
 import './css/bootstrap.min.css';
 
-/* import './css/app.css' */
 function App() {
+
+  const favMovies = localStorage.getItem('favs');
+
+  let tempMovieInFavs;
+
+  if (favMovies === null) {
+    tempMovieInFavs = [];
+  } else {
+    tempMovieInFavs = JSON.parse(favMovies);
+  }
+  console.log(tempMovieInFavs)
+
+  const addOrRemoveFromFavs = e => {
+    const btn = e.currentTarget;
+    const parent = btn.parentElement;
+    const imgURL = parent.querySelector('img').getAttribute('src');
+    const title = parent.querySelector('h5').innerText;
+    const overview = parent.querySelector('p').innerText;
+    const movieData = {
+      imgURL, title, overview,
+      id: btn.dataset.movieId
+    }
+    let movieIsInArray = tempMovieInFavs.find(oneMovie => {
+      return oneMovie.id === movieData.id
+    });
+    if (!movieIsInArray) {
+      tempMovieInFavs.push(movieData);
+      localStorage.setItem('favs', JSON.stringify(tempMovieInFavs));
+      console.log('se agrego la pelicula')
+    } else {
+      let moviesLeft = tempMovieInFavs.filter(oneMovie => {
+        return oneMovie.id !== movieData.id
+      });
+      localStorage.setItem('favs', JSON.stringify(moviesLeft));
+      console.log('se elimino la pelicula');
+    }
+
+
+  }
   return (
     <div className="container mt-3">
       <Header />
 
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/listado" component={ Listado } />
+        <Route path="/listado" render={ (props) => <Listado addOrRemoveFromFavs={ addOrRemoveFromFavs }{ ...props } /> } />
         <Route path="/detalle" component={ Detalle } />
-        <Route path="/resultados" component={ Resultados} />
+        <Route path="/resultados" component={ Resultados } />
 
       </Switch>
       <Footer />
--- src/components/Listado.js
+++ src/components/Listado.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 import swal from '@sweetalert/with-react';
 
 
-function Listado() {
+function Listado(props) {
     let token = sessionStorage.getItem('token');
+    
+   
 
     const [moviesList, setMoviesList] = useState([]);
 
@@ -36,6 +38,12 @@ console.log(moviesList);
                             <div className="col-3" key={idx} >
                                 <div className="card my-4">
                                     <img src={`https://image.tmdb.org/t/p/w500/${oneMovie.poster_path}`} className="card-img-top" alt="Detalle del Producto" />
+
+                                    <button className='favourite-btn'
+                                    onClick={props.addOrRemoveFromFavs}
+                                    data-movie-id={oneMovie.id}
+                                    >🖤</button>
+
                                     <div className="card-body">
                                         <h5 className="card-title">{oneMovie.title.substring(0, 30)}...</h5>
                                         <p className="card-text">{oneMovie.overview.substring(0, 100)}</p>
